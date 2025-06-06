a7a733e939de02d4aefc65eca8619f1e197bfe71
FedeBacelar <fedebacelar@gmail.com>
2025-06-05 23:02:16 -0300

    VoltixFlow - 1.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ToastContainerComponent} from "./shared/components/toast-container.component";
-import {AuthService} from "./core/services/auth.service";
-import {ToastService} from "./shared/services/toast/toast.service";
-
 @Component({
   selector: 'app-root',
   standalone: true,
